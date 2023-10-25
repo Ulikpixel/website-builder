61f20f504876e7855843073210399d7729face78
Ivan <frontend@imin.pro>
2023-10-25 23:54:57 +0600

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NextPage } from 'next'
+import MainHeading from '@/components/main-heading/main-heading'
+
+const Home: NextPage = () => (
+  <MainHeading>
+    <div>hello</div>
+  </MainHeading>
+)
+
+export default Home
