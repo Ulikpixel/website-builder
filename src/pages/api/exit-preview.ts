114fa8c703ac521d02cb50d569765f0e70919209
Ivan <frontend@imin.pro>
2023-11-11 00:26:56 +0600

    fix: 🐛 2 попытка решить баг с visual editor storyblok (данные обновляется только 1 раз)

--- src/pages/api/exit-preview.ts
+++ src/pages/api/exit-preview.ts
@@ -1,14 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function exit(req: NextApiRequest, res: NextApiResponse) {
-  res.clearPreviewData()
   const cookies = res.getHeader('Set-Cookie') as string[]
   res.setHeader(
     'Set-Cookie',
     cookies.map((cookie) =>
-      cookie.replace('SameSite=Lax', 'SameSite=None;Secure')
+      cookie.replace('SameSite=Lax', 'SameSite=None')
     )
   )
+
+  res.clearPreviewData()
 
   const { slug } = req.query
 
--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -7,17 +7,19 @@ export default async function preview(req: NextApiRequest, res: NextApiResponse)
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  res.setPreviewData({})
   const cookies = res.getHeader('Set-Cookie') as string[]
   res.setHeader(
     'Set-Cookie',
     cookies.map((cookie) =>
-      cookie.replace('SameSite=Lax', 'SameSite=None;Secure')
+      cookie.replace('SameSite=Lax', 'SameSite=None')
     )
   )
+
+  res.setPreviewData({})
 
   const updatedSlug = slug === 'home' ? '' : slug
 
   res.redirect(`/${updatedSlug}`)
   return res.end()
 }
+
