ed6bf9ae681485d4784acad3f6870165497ae6c7
Ivan <frontend@imin.pro>
2024-01-25 19:29:44 +0600

    feat: ✨ исправил ошибку от vercel

--- src/components/Molecules/Organick/Product/Product.tsx
+++ src/components/Molecules/Organick/Product/Product.tsx
@@ -13,7 +13,7 @@ const Product: FC<ProductProps> = ({ blok, idx }) => (
       ' flex items-center justify-center w-full mx-auto max-w-product h-product-small sm:h-product-big lg:m-auto lg:w-full',
       idx % 2 === 0 ? 'md:mr-auto md:ml-0' : 'md:ml-auto md:mr-0',
     )}
-    style={{ backgroundImage: `url(${blok.cover?.filename})` }}
+    style={{ backgroundImage: `url(${blok.cover.filename})` }}
   >
     <div className='bg-default-white py-5 px-10 rounded-2xl'>
       <p className='text-2xl text-organick-blue-dark font-medium'>{blok.title}</p>
