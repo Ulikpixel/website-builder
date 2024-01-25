6508aece335471f4b9b91ef40cc0811ec602f924
Ivan <frontend@imin.pro>
2024-01-25 19:24:59 +0600

    feat: ✨ update

--- src/types/storyblok-bloks/storyblok-bloks-types.ts
+++ src/types/storyblok-bloks/storyblok-bloks-types.ts
@@ -1,3 +1,4 @@
+
 export interface AssetStoryblok {
   alt?: string;
   copyright?: string;
@@ -20,59 +21,59 @@ export interface AdvertisingCardStoryblok {
 
 export type MultilinkStoryblok =
   | {
-    id?: string;
-    cached_url?: string;
-    anchor?: string;
-    linktype?: 'story';
-    story?: {
-      name: string;
-      created_at?: string;
-      published_at?: string;
-      id: number;
-      uuid: string;
-      content?: {
+      id?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: 'story';
+      story?: {
+        name: string;
+        created_at?: string;
+        published_at?: string;
+        id: number;
+        uuid: string;
+        content?: {
+          [k: string]: any;
+        };
+        slug: string;
+        full_slug: string;
+        sort_by_date?: null | string;
+        position?: number;
+        tag_list?: string[];
+        is_startpage?: boolean;
+        parent_id?: null | number;
+        meta_data?: null | {
+          [k: string]: any;
+        };
+        group_id?: string;
+        first_published_at?: string;
+        release_id?: null | number;
+        lang?: string;
+        path?: null | string;
+        alternates?: any[];
+        default_full_slug?: null | string;
+        translated_slugs?: null | any[];
         [k: string]: any;
       };
-      slug: string;
-      full_slug: string;
-      sort_by_date?: null | string;
-      position?: number;
-      tag_list?: string[];
-      is_startpage?: boolean;
-      parent_id?: null | number;
-      meta_data?: null | {
-        [k: string]: any;
-      };
-      group_id?: string;
-      first_published_at?: string;
-      release_id?: null | number;
-      lang?: string;
-      path?: null | string;
-      alternates?: any[];
-      default_full_slug?: null | string;
-      translated_slugs?: null | any[];
+      [k: string]: any;
+    }
+  | {
+      url?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: 'asset' | 'url';
+      [k: string]: any;
+    }
+  | {
+      email?: string;
+      linktype?: 'email';
       [k: string]: any;
     };
-    [k: string]: any;
-  }
-  | {
-    url?: string;
-    cached_url?: string;
-    anchor?: string;
-    linktype?: 'asset' | 'url';
-    [k: string]: any;
-  }
-  | {
-    email?: string;
-    linktype?: 'email';
-    [k: string]: any;
-  };
 
 export interface ButtonStoryblok {
   text: string;
   variant?: 'blue-dark' | 'white' | 'yellow';
   icon?: boolean;
-  link: Exclude<MultilinkStoryblok, { linktype?: 'email' } | { linktype?: 'asset' }>;
+  link: Exclude<MultilinkStoryblok, {linktype?: 'email'} | {linktype?: 'asset'}>;
   _uid: string;
   component: 'button';
   [k: string]: any;
@@ -82,7 +83,7 @@ export interface ContainerStoryblok {
   variants: 'small' | 'medium' | 'large' | 'xlarge';
   background?: 'blue-dark' | 'green' | 'dark' | 'grey' | 'green-opacity';
   grid?: '2' | '3' | '4';
-  content: (AdvertisingCardStoryblok | ProductStoryblok | VegetableStoryblok)[];
+  content: (VegetableStoryblok | AdvertisingCardStoryblok | ProductStoryblok)[];
   _uid: string;
   component: 'container';
   [k: string]: any;
@@ -100,8 +101,8 @@ export interface HeroStoryblok {
 
 export interface OrganickStoryblok {
   title: string;
-  description: string;
   content: (HeroStoryblok | ContainerStoryblok | VegetableSliderStoryblok)[];
+  description: string;
   _uid: string;
   component: 'organick';
   [k: string]: any;
@@ -109,7 +110,7 @@ export interface OrganickStoryblok {
 
 export interface ProductStoryblok {
   title: string;
-  cover?: AssetStoryblok;
+  cover: AssetStoryblok;
   _uid: string;
   component: 'product';
   [k: string]: any;
